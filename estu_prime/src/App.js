8bbcb39240adc8b71b043796de14ffab616416ce
jAiguiser <jhonumss@gmail.com>
2024-04-07 09:48:00 -0400

    footer

--- estu_prime/src/App.js
+++ estu_prime/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import NavB from './components/navbar';
-import Header from './components/header'
+import Header from './components/header';
+import Footer from './components/footer';
 
 const isLogged = true;
 function App() {
   return (
     <>
-     <Header/>
-     <NavB/>
+      <Header/>
+      <NavB/>
+      <Footer/>
     </>
   );
 }
