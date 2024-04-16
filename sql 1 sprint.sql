2955353dd38725f159536cf5e9c565486e37266a
Ethan Pierce Gerl Serelis <94498054+Eth4nSP@users.noreply.github.com>
2024-04-16 19:27:10 -0400

    Base del primer Sprint

--- sql 1 sprint.sql
+++ sql 1 sprint.sql
@@ -11,6 +11,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema estuprime
 -- -----------------------------------------------------
 
+
 -- -----------------------------------------------------
 -- Schema estuprime
 -- -----------------------------------------------------
