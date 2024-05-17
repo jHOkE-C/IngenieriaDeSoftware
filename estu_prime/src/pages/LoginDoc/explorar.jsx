4069d6e8ca76ff5ef5e947604c9967b55b728d93
jAiguiser <jhonumss@gmail.com>
2024-05-17 00:23:55 -0400

    busqueda

--- estu_prime/src/pages/LoginDoc/explorar.jsx
+++ estu_prime/src/pages/LoginDoc/explorar.jsx
@@ -1,18 +1,26 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
 function ExplorarD() {
-  const [resultados, setResultados] = useState()
+  const [resultados, setResultados] = useState(null)
   const [buscado, setBuscado] = useState()
-  const buscarDatos = () =>{
+  const buscarDatos = (e) =>{
     
   }
   return (
     <ExplorarContainer>
-      <form>
+      <form className='boxS'>
         <input type="text" 
+          className='boxS__search'
           value={buscado}
           onChange={buscarDatos}
         />
+        <div className="resultados">
+          {resultados === null ? 
+          <h5>Búsqueda no encontrada...</h5> 
+          : 
+          null
+          }
+        </div>
       </form>
     </ExplorarContainer>
   )
@@ -21,5 +29,34 @@ function ExplorarD() {
 export default ExplorarD
 
 const ExplorarContainer = styled.nav`
-  
+  display: flex;
+  justify-content: space-around;
+  min-height: 90vh;
+  .boxS{
+    position: relative;
+    width: 35vw;
+    height: 3vw;
+  }
+  .boxS__search{
+    margin-top: 0.3vw;
+    background-color: #F2E9E4;
+    border: none;
+    border: 1px solid black;
+    border-radius: 3px;
+    width: 100%;
+    height: 70%;
+    padding: 1%;
+  }
+  .resultados{
+      width: 100%;
+      position: absolute;
+      background-color: #F2E9E4;
+      border: 1px solid black;
+      border-radius: 3px;
+      visibility: hidden;
+      padding: 1%;
+  }
+  .boxS__search:focus-visible + .resultados{
+    visibility: visible;
+  } 
 `
