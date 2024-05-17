c20f0a883256b1cd86cd88b4eab7f8f2d284c6a3
jAiguiser <jhonumss@gmail.com>
2024-05-17 00:40:29 -0400

    buscador

--- estu_prime/src/pages/LoginDoc/explorar.jsx
+++ estu_prime/src/pages/LoginDoc/explorar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
+import SearchSVG from '../../assents/svg/search.svg'
 function ExplorarD() {
   const [resultados, setResultados] = useState(null)
   const [buscado, setBuscado] = useState()
@@ -8,21 +9,26 @@ function ExplorarD() {
   }
   return (
     <ExplorarContainer>
-      <form className='boxS'>
-        <input type="text" 
-          className='boxS__search'
-          value={buscado}
-          onChange={buscarDatos}
-        />
-        <div className="resultados">
-          {resultados === null ? 
-          <h5>Búsqueda no encontrada...</h5> 
-          : 
-          resultados.map((resultado)=>{
-            
-          })
-          }
+      <form className='formS'>
+        <div className='boxS'>
+          <input 
+            type="text" 
+            className='boxS__search'
+            value={buscado}
+            onChange={buscarDatos}
+          ></input>  
+          <div className="resultados">
+            {resultados === null ? 
+            <h5>Búsqueda no encontrada...</h5> 
+            : 
+            resultados.map((resultado)=>{
+                        
+            })
+            }
+          </div>
+          <img src={SearchSVG} alt="" className='boxS__img'/> 
         </div>
+        
       </form>
     </ExplorarContainer>
   )
@@ -34,10 +40,13 @@ const ExplorarContainer = styled.nav`
   display: flex;
   justify-content: space-around;
   min-height: 90vh;
-  .boxS{
+  .formS{
     position: relative;
     width: 35vw;
     height: 3vw;
+  }
+  .boxS{
+    display: block;
   }
   .boxS__search{
     margin-top: 0.3vw;
@@ -48,6 +57,12 @@ const ExplorarContainer = styled.nav`
     width: 100%;
     height: 70%;
     padding: 1%;
+  }
+  .boxS__img{
+    position: absolute;
+    height: 20px;
+    top: 0.5vw;
+    left: 97%;
   }
   .resultados{
       width: 100%;
