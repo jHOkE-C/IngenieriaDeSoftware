88cbe1ed6940d1e4e87434b4fa29720b3e260e08
Eth4nSP <eth4n.gerl@gmail.com>
2024-05-29 00:42:45 -0400

    EliminarCurso

---
+++ estu_prime/src/api/cursoEliminar.php
@@ -0,0 +1,17 @@
+<?php
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+header('Access-Control-Allow-Origin: ' . $origin);
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Credentials: true');
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "estuprime";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Error de conexión: " . $conn->connect_error);
+}
+$json_data = file_get_contents("php://input");
+$data = json_decode($json_data, true);
